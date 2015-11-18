d45318f284f585b57602c683c380a86e2af0b9aa
Jeremie <lucheng989898@gmail.com>
2015-11-19 02:52:07 +0800

    写了一个有解算法 但是实践证明难度不高

--- src/asia/jeremie/Main.java
+++ src/asia/jeremie/Main.java
@@ -1,10 +1,28 @@
 package asia.jeremie;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // 随机书生成器测试
+//        int m = 1;
+//        int[] a = new int[m];
+//        for (int i = 0; i != 500; ++i) {
+//            ++a[Core.random(m - 1)];
+//        }
+//        for (int i = 0; i < m; i++) {
+//            System.out.print(" " + a[i]);
+//        }
+
+        Core core = new Core();
+        System.out.println(core.initial(16, 16, 99, true));
+        System.out.println(core.Hit(2, 2));
+        for (int i = 0; i < core.data.length; i++) {
+            for (int j = 0; j < core.data[i].length; j++) {
+                System.out.print("\t" + core.data[i][j].toInt());
+            }
+            System.out.println();
+        }
+
     }
 }
